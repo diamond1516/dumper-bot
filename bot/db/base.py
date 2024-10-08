16e8571c89d063f03078dda1b9747249fbfb1ebc
user <ahmadboyabdurahimov589@gmail.com>
2024-10-08 17:50:53 +0500

    initial commit

--- bot/db/base.py
+++ bot/db/base.py
@@ -1,3 +1,14 @@
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+from sqlalchemy import Integer
 
-Base = declarative_base()
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        name = str(cls.__name__)
+        return ''.join(['_' + c.lower() if c.isupper() else c for c in name]).lstrip('_') + 's'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+
