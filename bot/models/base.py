259e5dcad324f55c8fbe8ccdf5e58ad405c2532a
user <ahmadboyabdurahimov589@gmail.com>
2024-10-09 15:48:13 +0500

    initial commit

--- bot/models/base.py
+++ bot/models/base.py
@@ -7,8 +7,6 @@ class Base(DeclarativeBase):
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
-        name = str(cls.__name__)
-        return ''.join(['_' + c.lower() if c.isupper() else c for c in name]).lstrip('_') + 's'
-
+        return cls.__name__.lower() + 's'
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
 
