6ee33b5cc48b23821c2fddb246dfab686b04841c
user <ahmadboyabdurahimov589@gmail.com>
2025-02-22 19:40:11 +0500

    initial commit

--- bot/handlers/add_db.py
+++ bot/handlers/add_db.py
@@ -11,49 +11,49 @@ router = Router(
 
 
 @router.message(AddDB.project_name)
-async def handle_project_name(message: Message, session: AsyncSession, state: FSMContext):
+async def handle_project_name(message: Message, state: FSMContext):
     await state.update_data(project_name=message.text)
     await message.answer('DB name: ')
     await state.set_state(AddDB.name)
 
 
 @router.message(AddDB.name)
-async def handle_project_name(message: Message, session: AsyncSession, state: FSMContext):
+async def handle_project_name(message: Message, state: FSMContext):
     await state.update_data(name=message.text)
     await message.answer('DB password: ')
     await state.set_state(AddDB.password)
 
 
 @router.message(AddDB.password)
-async def handle_project_name(message: Message, session: AsyncSession, state: FSMContext):
+async def handle_project_name(message: Message, state: FSMContext):
     await state.update_data(password=message.text)
     await message.answer('DB username: ')
     await state.set_state(AddDB.user)
 
 
 @router.message(AddDB.user)
-async def handle_project_name(message: Message, session: AsyncSession, state: FSMContext):
+async def handle_project_name(message: Message, state: FSMContext):
     await state.update_data(user=message.text)
     await message.answer('DB host: ')
     await state.set_state(AddDB.host)
 
 
 @router.message(AddDB.host)
-async def handle_project_name(message: Message, session: AsyncSession, state: FSMContext):
+async def handle_project_name(message: Message, state: FSMContext):
     await state.update_data(host=message.text)
     await message.answer('DB port: ')
     await state.set_state(AddDB.port)
 
 
 @router.message(AddDB.port)
-async def handle_project_name(message: Message, session: AsyncSession, state: FSMContext):
+async def handle_project_name(message: Message, state: FSMContext):
     await state.update_data(port=message.text)
     await message.answer('DB interval: ')
     await state.set_state(AddDB.interval)
 
 
 @router.message(AddDB.interval)
-async def handle_project_name(message: Message, session: AsyncSession, state: FSMContext):
+async def handle_project_name(message: Message, state: FSMContext):
     await state.update_data(interval=message.text)
     await message.answer('DB api: ')
     await state.set_state(AddDB.api)
