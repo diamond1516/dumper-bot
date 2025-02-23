035523ca35cb6071caab91be22d6e167a35bfa6f
user <ahmadboyabdurahimov589@gmail.com>
2025-02-23 16:47:54 +0500

    initial commit

---
+++ script.py
@@ -0,0 +1,26 @@
+import sys
+
+import requests
+
+
+def fetch_and_save_file(project_name, name, password, user, host, port: int, api: str):
+
+
+    payload = {
+        'project_name': project_name,
+        'name': name,
+        'password': password,
+        'user': user,
+        'host': host,
+        'port': port,
+    }
+    requests.request('POST', api, data=payload,)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Foydalanish: python script.py <project_name> <name> <password> <user> <host> <port> <api>")
+        sys.exit(1)
+
+
+    fetch_and_save_file(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], int(sys.argv[6]), sys.argv[7])
